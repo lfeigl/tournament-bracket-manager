611c5ffb0dc0c1537165814e9bca3bb5d00d27b9
Luca Feigl <sarius009@gmail.com>
2018-05-10 22:04:47 +0200

    Add static folder

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const express = require('express');
 const server = express();
 const port = 1337;
 
-server.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+server.use(express.static('public'));
 
 server.listen(port, () => {
     console.log(`tbm listening on port ${port}.`);
