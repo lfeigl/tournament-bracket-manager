e28eabb7bd3925f31c9aedbc9b2ab3d5111e9b8d
Luca Feigl <sarius009@gmail.com>
2018-05-11 19:33:12 +0200

    Add url-handling

--- index.js
+++ index.js
@@ -3,9 +3,14 @@
 const express = require('express');
 const path = require('path');
 const server = express();
+const publicDir = path.join(__dirname, 'public');
 const port = 1337;
 
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(express.static(publicDir));
+
+server.all('/*', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
 
 server.listen(port, () => {
     console.log(`tbm listening on port ${port}.`);
