4e53639e49fc1b59dd97d54391439f411f330cc6
Luca Feigl <sarius009@gmail.com>
2018-05-11 17:00:32 +0200

    Add sample routing

--- app/index.js
+++ app/index.js
@@ -1,4 +1,19 @@
 const angular = require('angular');
-const app = angular.module('tbm', []);
+const app = angular.module('tbm', [
+    require('angular-route'),
+]);
+
+app.config(($locationProvider, $routeProvider) => {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false,
+    });
+
+    $routeProvider.when('/', {
+        controller: 'HomeCtrl',
+        controllerAs: 'home',
+        template: require('./home/tpl.html'),
+    });
+});
 
 require('./home')(app);
