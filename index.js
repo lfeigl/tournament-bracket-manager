e2f7fa420b6b3d51a0584ba2ff3bef08eb0aa7f3
Luca Feigl <sarius009@gmail.com>
2018-05-11 15:08:27 +0200

    Use path module for static folder

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* eslint no-console: 0 */
 
 const express = require('express');
+const path = require('path');
 const server = express();
 const port = 1337;
 
-server.use(express.static('public'));
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.listen(port, () => {
     console.log(`tbm listening on port ${port}.`);
