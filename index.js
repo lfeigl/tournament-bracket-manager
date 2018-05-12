9b24867d1c98f32208c7340ed3102a9ab5969707
Luca Feigl <sarius009@gmail.com>
2018-05-12 14:00:46 +0200

    Add sample mongodb connection & Add arguments to start script

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 /* eslint no-console: 0 */
 
 const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
 const path = require('path');
+const parseArgs = require('minimist');
+
 const server = express();
 const publicDir = path.join(__dirname, 'public');
-const port = 1337;
+const args = parseArgs(process.argv.slice(2));
+const port = args.port;
+const mongoUrl = args.mongodb;
+const mongoOpts = { useNewUrlParser: true };
+
+mongoClient.connect(`mongodb://${mongoUrl}`, mongoOpts, (err, client) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log(`tbm connected to database at ${mongoUrl}.`);
+        const db = client.db('tbmdb');
+        const collection = db.collection('test');
+
+        collection.find({}).toArray((err, docs) => {
+            console.log(docs);
+        });
+    }
+});
 
 server.use(express.static(publicDir));
 
