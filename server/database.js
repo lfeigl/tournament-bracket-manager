35719ac3d66149b71fa14a1e2deef0e023784690
Luca Feigl <sarius009@gmail.com>
2018-05-12 15:20:58 +0200

    Add new error handling

--- server/database.js
+++ server/database.js
@@ -1,19 +1,21 @@
+/* eslint no-console: 0 */
+
 const mongoClient = require('mongodb').MongoClient;
 const opts = { useNewUrlParser: true };
 
 function connect (url) {
     mongoClient.connect(`mongodb://${url}`, opts, (err, client) => {
         if (err) {
-            console.error(err);
-        } else {
-            console.log(`tbm connected to database at ${url}.`);
-            const db = client.db('tbmdb');
-            const collection = db.collection('test');
-
-            collection.find({}).toArray((err, docs) => {
-                console.log(docs);
-            });
+            throw err;
         }
+
+        console.log(`tbm connected to database at ${url}.`);
+        const db = client.db('tbmdb');
+        const collection = db.collection('test');
+
+        collection.find({}).toArray((err, docs) => {
+            console.log(docs);
+        });
     });
 }
 
