8e9d642f65ea02093dcd45d4478d7cdbea19830e
Luca Feigl <sarius009@gmail.com>
2018-05-12 14:51:13 +0200

    Move database logic to seperate file

--- server/index.js
+++ server/index.js
@@ -1,30 +1,15 @@
 /* eslint no-console: 0 */
 
 const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
 const path = require('path');
 const parseArgs = require('minimist');
+const database = require('./database.js');
 
 const server = express();
 const publicDir = path.join(__dirname, 'public');
 const args = parseArgs(process.argv.slice(2));
 const port = args.port;
 const mongoUrl = args.mongodb;
-const mongoOpts = { useNewUrlParser: true };
-
-mongoClient.connect(`mongodb://${mongoUrl}`, mongoOpts, (err, client) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`tbm connected to database at ${mongoUrl}.`);
-        const db = client.db('tbmdb');
-        const collection = db.collection('test');
-
-        collection.find({}).toArray((err, docs) => {
-            console.log(docs);
-        });
-    }
-});
 
 server.use(express.static(publicDir));
 
@@ -34,4 +19,5 @@ server.all('/*', (req, res) => {
 
 server.listen(port, () => {
     console.log(`tbm listening on port ${port}.`);
+    database.connect(mongoUrl);
 });
