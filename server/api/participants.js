72656a487e447aacacbc67724665b0fdb62a8f0e
Luca Feigl <sarius009@gmail.com>
2018-05-13 19:52:04 +0200

    Use express error handler

--- server/api/participants.js
+++ server/api/participants.js
@@ -1,10 +1,12 @@
 const db = require('../mongodb.js');
 
 module.exports = {
-    getAll: function (req, res) {
+    getAll: function (req, res, next) {
         const collection = db.get('participants');
 
         collection.find().toArray((err, participants) => {
+            if (err) next(err);
+
             res.send(participants);
         });
     },
