8d2fc4ec0be75cc1c3127a28ead6c5c656a7ddf0
Luca Feigl <sarius009@gmail.com>
2018-05-13 19:51:17 +0200

    Add express error handler

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,5 @@
+/* eslint no-unused-vars: 0 */
+
 const expressRouter = require('express').Router;
 const router = expressRouter();
 
@@ -9,6 +11,11 @@ router.get('/participants', participants.getAll);
 
 router.all('/*', (req, res) => {
     res.sendStatus(404);
+});
+
+router.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err);
 });
 
 module.exports = router;
