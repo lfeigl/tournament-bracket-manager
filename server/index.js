43a8f9a71c7920d53db1a9f8ec7dbefccdc02816
Luca Feigl <sarius009@gmail.com>
2018-05-13 11:59:04 +0200

    Move logging to index.js

--- server/index.js
+++ server/index.js
@@ -18,9 +18,10 @@ server.all('/*', (req, res) => {
     res.sendFile(path.join(publicDir, 'index.html'));
 });
 
-mongodb.connect((err) => {
+mongodb.connect((err, mongoConfig) => {
     if (err) throw err;
 
+    console.log(`tbm connected to ${mongoConfig.dbName} at ${mongoConfig.server} on port ${mongoConfig.port}.`);
     server.listen(port, () => {
         console.log(`tbm listening on port ${port}.`);
     });
--- server/mongodb.js
+++ server/mongodb.js
@@ -1,25 +1,20 @@
 /* eslint no-console: 0 */
 
 const MongoClient = require('mongodb').MongoClient;
-const config = require('../tbm.config.js');
+const mongoConfig = require('../tbm.config.js').mongoDB;
 
-const server = config.mongoDB.server;
-const port = config.mongoDB.port;
-const dbName = config.mongoDB.dbName;
 const opts = { useNewUrlParser: true };
-
 let db = null;
 
 module.exports = {
     connect: function (done) {
-        if (db) return done();
+        if (db) return done(null, mongoConfig);
 
-        MongoClient.connect(`mongodb://${server}:${port}`, opts, (err, client) => {
+        MongoClient.connect(`mongodb://${mongoConfig.server}:${mongoConfig.port}`, opts, (err, client) => {
             if (err) return done(err);
 
-            console.log(`tbm connected to ${dbName} at ${server} on port ${port}.`);
-            db = client.db(dbName);
-            done();
+            db = client.db(mongoConfig.dbName);
+            done(null, mongoConfig);
         });
     },
     get: function () {
