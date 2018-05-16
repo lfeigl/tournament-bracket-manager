17c44d211a1456342824d59b5380e5a5a5da9fdb
Luca Feigl <sarius009@gmail.com>
2018-05-16 21:51:11 +0200

    Add get request & Add promise handlers

--- app/participants/ctrl.js
+++ app/participants/ctrl.js
@@ -2,6 +2,18 @@ module.exports = (app) => {
     app.controller('ParticipantsCtrl', function (ParticipantsSrvc) {
         const vm = this;
 
-        vm.add = ParticipantsSrvc.add;
+        vm.add = add;
+
+        ParticipantsSrvc.getAll().then((res) => {
+            vm.all = res.data;
+        }).catch((err) => {
+            console.error(err);
+        });
+
+        function add (participant) {
+            ParticipantsSrvc.add(participant).catch((err) => {
+                console.error(err);
+            });
+        }
     });
 };
--- app/participants/srvc.js
+++ app/participants/srvc.js
@@ -2,10 +2,15 @@ module.exports = (app) => {
     app.service('ParticipantsSrvc', function ($http) {
         const srvc = this;
 
+        srvc.getAll = getAll;
         srvc.add = add;
 
+        function getAll () {
+            return $http.get('/api/participants');
+        }
+
         function add (participant) {
-            $http.post('/api/participants', participant);
+            return $http.post('/api/participants', participant);
         }
     });
 };
