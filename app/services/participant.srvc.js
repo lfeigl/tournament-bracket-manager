50bb5c188d7b82899fbfdb2e2e8448776af2c0ac
Luca Feigl <sarius009@gmail.com>
2018-05-25 23:07:49 +0200

    Change getOne function to getDetails function

--- app/services/participant.srvc.js
+++ app/services/participant.srvc.js
@@ -3,19 +3,19 @@ module.exports = (app) => {
         const srvc = this;
 
         srvc.getAll = getAll;
-        srvc.getOne = getOne;
         srvc.add = add;
+        srvc.getDetails = getDetails;
 
         function getAll () {
             return $http.get('/api/participants');
         }
 
-        function getOne (id) {
-            return $http.get(`/api/participants/${id}`);
-        }
-
         function add (participant) {
             return $http.post('/api/participants', participant);
+        }
+
+        function getDetails (ids) {
+            return $http.post('/api/participants/details', ids);
         }
     });
 };
