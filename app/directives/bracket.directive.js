0a2c957920008aebf9892effae3d68e4cee733fa
Luca Feigl <sarius009@gmail.com>
2018-08-04 00:33:38 +0200

    Add sample bracket directive

---
+++ app/directives/bracket.directive.js
@@ -0,0 +1,35 @@
+const _ = require('lodash');
+const $ = require('jquery');
+
+module.exports = app => {
+    app.directive('bracket', () => {
+        return {
+            link: (scope, element, attrs) => {
+                const id = _.head(element).id;
+                const data = {
+                    teams: [
+                        [ 'Team A', 'Team B' ],
+                        [ 'Team C', 'Team D' ],
+                    ],
+                    results: [
+                        [
+                            [ 1, 2 ],
+                            [ 3, 4 ],
+                        ],
+                        [
+                            [ 5, 6 ],
+                            [ 7, 8 ],
+                        ],
+                    ],
+                };
+
+                $(() => {
+                    $(`#${id}`).bracket({
+                        init: data,
+                        disableHighlight: true,
+                    });
+                });
+            },
+        };
+    });
+};
