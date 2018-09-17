57e30064898c9d473ba43d4dc505ce43f5cd5459
Luca Feigl <sarius009@gmail.com>
2018-09-17 18:55:04 +0200

    Add error layout

--- app/pages/error/controller.js
+++ app/pages/error/controller.js
@@ -1,12 +1,15 @@
 const _ = require('lodash');
 
 module.exports = app => {
-    app.controller('ErrorHandlerCtrl', function ($location, ErrorHandlerSrvc) {
+    app.controller('ErrorHandlerCtrl', function ($rootScope, $location, ErrorHandlerSrvc) {
         const vm = this;
 
         vm.err = ErrorHandlerSrvc.err;
 
-        if (_.isNull(vm.err)) {
+        if (!_.isNull(vm.err)) {
+            vm.formatted = JSON.stringify(vm.err, null, 4);
+            $rootScope.pageTitle = `${vm.err.status} ${vm.err.statusText}`;
+        } else {
             $location.url('/home');
         }
     });
