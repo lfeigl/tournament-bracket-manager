587e610afb20b82db1f36af3ee51894506ddc94b
Luca Feigl <sarius009@gmail.com>
2018-10-04 20:07:09 +0200

    Add script for generating config file

---
+++ scripts/generate-tbm-config.js
@@ -0,0 +1,33 @@
+/* eslint no-console: 0 */
+
+const path = require('path');
+const fs = require('fs');
+const configPath = path.join(__dirname, '..', 'tbm.config.js');
+const template = [
+    'const config = require(\'./tbm.config.defaults.js\');',
+    'module.exports = config;',
+    '',
+    '/* add your settings below this */',
+    '',
+    '',
+];
+
+fs.access(configPath, err => {
+    if (err) {
+        if (err.code === 'ENOENT') {
+            generateConfig();
+        } else {
+            throw err;
+        }
+    } else {
+        console.log('Config file already exists. It will not be generated.');
+    }
+});
+
+function generateConfig () {
+    fs.writeFile(configPath, template.join('\n'), err => {
+        if (err) throw err;
+
+        console.log('Generated config file.');
+    });
+}
